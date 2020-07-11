c60178802f3122a9fc37dacc4d68919db0e82b2e
charlie yin <charlie.t.yin@gmail.com>
2020-07-11 15:41:15 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -9,17 +9,14 @@ function Spinner() {
   const spinningCube = useRef();
 
   useFrame(() => {});
-
   const [expand, setExpand] = useState(false);
-  const props = useSpring({
-    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
-  });
+  const props = useSpring({ scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1] });
 
   return (
     <a.mesh
+      ref={spinningCube}
       onClick={() => setExpand(!expand)}
       scale={props.scale}
-      ref={spinningCube}
       position={[0, 1, 0]}>
       <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
       <meshNormalMaterial attach="material" color={'#4FFF9C'} />
